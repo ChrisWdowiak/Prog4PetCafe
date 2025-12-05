81439c3ddf33a9b270cf5e5f013f6b7216104f96
Chris Wdowiak <cwdowiakjr@gmail.com>
2025-12-05 14:56:48 -0800

    small changes idk

--- Prog4PetCafe/src/Application/App.java
+++ Prog4PetCafe/src/Application/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-//import Requests.*;
+import Requests.*;
 
 /*=============================================================================
  |   Assignment:  Program #4:  Database Design and Implementation
